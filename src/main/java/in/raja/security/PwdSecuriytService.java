3a0e1192526f14a575e76085499332e75c96ddee
Rajasekhar <rajasekhar.kds@gmail.com>
2022-08-06 18:29:50 +0530

    initial commit

---
+++ src/main/java/in/raja/security/PwdSecuriytService.java
@@ -0,0 +1,17 @@
+package in.raja.security;
+
+import java.util.Base64;
+import java.util.Base64.Encoder;
+
+public class PwdSecuriytService {
+	
+	public String encode(String text) {
+		
+		Encoder encoder=Base64.getEncoder();
+		
+		String encodeToString = encoder.encodeToString(text.getBytes());
+		return encodeToString;
+				
+	}
+
+}
